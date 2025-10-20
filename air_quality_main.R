8d4766e2f835850b2a6be9b4e39f5baec66933dc
zereim <wtruskolaski@gmail.com>
2025-10-19 23:41:35 -0400

    changing plot

--- air_quality_main.R
+++ air_quality_main.R
@@ -1,9 +1,14 @@
 library(tidyverse)
 
 airquality %>% 
-  ggplot(aes(Ozone, Wind))+
-  geom_point()+
-  geom_smooth(method="lm")
+  ggplot(aes(Ozone, Wind)) +
+  geom_point() +
+  geom_smooth(method='lm') +
+  theme_minimal() +
+  labs(x='Ozone (ppb)', y='Wind (mph)', title='Linear Regression of Wind speed vs. 
+       Ozone concentration') +
+  theme(plot.title = element_text(hjust=0.5, size=20, face='bold')) 
   
+
 
 
